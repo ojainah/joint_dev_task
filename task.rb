b35c2babfc2e06314f1403f34e0b2fd74d639d7c
Lancai <araraaraiguma@gmail.com>
2020-03-05 22:58:58 +0900

    Q7修正&Q11~15回答致しました

--- task.rb
+++ task.rb
@@ -44,7 +44,6 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
   p numbers2 = numbers1.map{ |n| n*10 }
 end
 
@@ -52,7 +51,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  p array.map(&:to_i)
+  p array.map!(&:to_i)
 end
 
 def q8
@@ -87,14 +86,16 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten.uniq.each.with_index(1) do |sport, index|
+    puts "No.#{index} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -102,14 +103,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  puts user_data.merge(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data.keys
 end
 
 def q15
@@ -117,7 +118,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.include?(:age) ? "OK" : "NG"
+  puts data2.include?(:age) ? "OK" : "NG"
 end
 
 def q16
